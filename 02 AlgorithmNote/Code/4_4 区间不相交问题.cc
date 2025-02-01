803b03808d5a5b6efa88fdee5870d31edad41b25
RedMi-MMM <m_lilonglong@qq.com>
2025-02-01 09:01:20 +0800

    完成贪心算法一道

--- 02 AlgorithmNote/Code/4_4 区间不相交问题.cc
+++ 02 AlgorithmNote/Code/4_4 区间不相交问题.cc
@@ -3,6 +3,7 @@
 //
 // ========================
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 const int max_n = 10001;
@@ -13,13 +14,28 @@ struct OpenInterval {
 
 OpenInterval interval_array[max_n];
 
+static bool cmp (OpenInterval O1, OpenInterval O2) {
+  if (O1.x != O2.x) {
+    return O1.x > O2.x;
+  } else {
+    return O1.y < O2.y;
+  }
+}
 
-
-
-
-
-
-
-
-
-
+int main () {
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; ++i) {
+    cin >> interval_array[i].x >> interval_array[i].y;
+  }
+  sort(interval_array, interval_array + n, cmp);
+  int ans = 1, last_x = interval_array[0].x;
+  for (int i = 1; i < n; ++i) {
+    if (interval_array[i].y <= last_x) {
+      ans = ans + 1;
+      last_x = interval_array[i].x;
+    }
+  }
+  cout << ans;
+  return 0;
+}
